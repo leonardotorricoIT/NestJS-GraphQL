b617339fd8475761d79bf2cb95dedf58f033bc7d
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2025-08-19 11:55:05 -0400

    feat: add conection to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import databaseConfig from './database/database.config';
+import { DatabaseModule } from './database/database.module';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
+      driver: ApolloDriver,
       autoSchemaFile: true,
       installSubscriptionHandlers: true,
     }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
   ],
 })
 export class AppModule {}
