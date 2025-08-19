723c301691e83ed402374699984c6fbc0432ffdc
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2025-08-19 11:43:37 -0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      installSubscriptionHandlers: true,
+    }),
+  ],
+})
+export class AppModule {}
